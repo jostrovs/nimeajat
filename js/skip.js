6881bca838fe8e8cbfe686488c5f1073d4326c24
jori <jori.ostrovskij@netwheels.fi>
2020-10-11 16:17:24 +0300

    B-taso skip

--- js/skip.js
+++ js/skip.js
@@ -32,4 +32,12 @@ const COMMON_SKIP = [
     "vb2020n.TC.1", "vb2020n.TC.2", "vb2020n.TC.3", "vb2020n.TC.4", "vb2020n.TC.5", "vb2020n.TC.6", "vb2020n.TC.7", "vb2020n.TC.8", "vb2020n.TC.9", "vb2020n.TC.10", 
     "vb2020n.TC.11", "vb2020n.TC.12", "vb2020n.TC.13", "vb2020n.TC.14", "vb2020n.TC.15", "vb2020n.TC.16", "vb2020n.TC.17", "vb2020n.TC.18", "vb2020n.TC.19", "vb2020n.TC.20", 
     "vb2020n.TC.21", "vb2020n.TC.22", "vb2020n.TC.23", "vb2020n.TC.24", "vb2020n.TC.25", "vb2020n.TC.26", "vb2020n.TC.27", "vb2020n.TC.28", "vb2020n.TC.29", "vb2020n.TC.30", 
+
+    // B-pojat taso
+    "vb2020n.PB.1", "vb2020n.PB.2", "vb2020n.PB.3", "vb2020n.PB.4", "vb2020n.PB.5", "vb2020n.PB.6",  
+
+    // B-tytöt taso
+    "vb2020n.TB.1", "vb2020n.TB.2", "vb2020n.TB.3", "vb2020n.TB.4", "vb2020n.TB.5", "vb2020n.TB.6", "vb2020n.TB.7", "vb2020n.TB.8", "vb2020n.TB.9", "vb2020n.TB.10", 
+    "vb2020n.TB.11", "vb2020n.TB.12", "vb2020n.TB.13", "vb2020n.TB.14", "vb2020n.TB.15", "vb2020n.TB.16", "vb2020n.TB.17", "vb2020n.TB.18", "vb2020n.TB.19", "vb2020n.TB.20", 
+     
 ];
